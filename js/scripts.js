f4051d27e1a414b82e28b0d971b7e6f5a8f72c83
blitnee <brittneelc@gmail.com>
2017-02-24 12:05:39 -0500

    Add Cat Names
    Add Unique Cat Names, Changes to font

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
 
    //CAT 1
 	var $click__count1 = $('#click__count1');
-	var click1 = 0;
+   var $cat1__name = $('#cat1__name');
+   var click1 = 0;
 
-	$('#cat1').click(function() {
+   // Set cat1 name
+	$cat1__name.text('Kit');
+
+   $('#cat1').click(function() {
    		console.log('Kitty1 has been clicked!');
 
    		if (click1 === 100) {
@@ -20,7 +24,11 @@ $(document).ready(function() {
 
    // CAT 2
    var $click__count2 = $('#click__count2');
+   var $cat2__name = $('#cat2__name');
    var click2 = 0;
+
+   // Set cat2 name
+   $cat2__name.text('Kit');
 
    $('#cat2').click(function() {
          console.log('Kitty2 has been clicked!');

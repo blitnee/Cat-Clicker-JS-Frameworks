649df95644f183d3928985d285bf11775fb30156
blitnee <brittneelc@gmail.com>
2017-02-24 10:39:48 -0500

    Finished!
    Fully Functioning cat clicker

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	var $click__count = $('#click__count');
+	var click = 0;
+
+	$('#cat__image').click(function() {
+   		console.log('This kitty has been clicked!');
+
+   		if (click === 10) {
+   			alert('100 clicks... Get a life! Go read a book!');
+   		} else{
+   		   	// add a click on click
+   			click++;
+   			// change score
+   			$click__count.text(+ click);
+   		}
+   });
+
+
+});

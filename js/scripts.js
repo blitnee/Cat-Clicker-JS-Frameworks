65dc1d06083ee54105c0713d2c576328ca9b197b
blitnee <brittneelc@gmail.com>
2017-02-24 11:48:06 -0500

    Add 2nd cat
    Quickly added 2nd cat

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
 
-	var $click__count = $('#click__count');
-	var click = 0;
+   //CAT 1
+	var $click__count1 = $('#click__count1');
+	var click1 = 0;
 
-	$('#cat__image').click(function() {
-   		console.log('This kitty has been clicked!');
+	$('#cat1').click(function() {
+   		console.log('Kitty1 has been clicked!');
 
-   		if (click === 100) {
+   		if (click1 === 100) {
    			alert('100 clicks... Get a life! Go read a book!');
    		} else{
-   		   	// add a click on click
-   			click++;
+   		   // add a click on click
+   			click1++;
    			// change score
-   			$click__count.text(+ click);
+   			$click__count1.text(+ click1);
    		}
+   });
+
+
+   // CAT 2
+   var $click__count2 = $('#click__count2');
+   var click2 = 0;
+
+   $('#cat2').click(function() {
+         console.log('Kitty2 has been clicked!');
+
+         if (click2 === 100) {
+            alert('100 clicks... Get a life! Go read a book!');
+         } else{
+            // add a click on click
+            click2++;
+            // change score
+            $click__count2.text(+ click2);
+         }
    });
 
 
